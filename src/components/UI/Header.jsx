44a015be4cf839fce282321fdf17d523b6021f1d
AkashThakur14 <ak9988385532@gmail.com>
2025-03-09 02:09:09 +0530

    mobile responsive

--- src/components/UI/Header.jsx
+++ src/components/UI/Header.jsx
@@ -1,6 +1,16 @@
+import { GiHamburgerMenu } from "react-icons/gi"
 import { NavLink } from "react-router-dom"
+import { useState } from "react";
 
 export const Header = () => {
+    const [show,setShow] = useState(false);
+
+const handleButtonToggle = () =>{
+return(
+    setShow(!show)
+)
+}
+
     return (
 
         <header>
@@ -12,7 +22,7 @@ export const Header = () => {
                         </NavLink>
                     </div>
 
-                    <nav>
+                    <nav className={show ? "menu-mobile" : "menu-web"}>
                         <ul>
                             <li><NavLink to="/">Home</NavLink></li>
                             <li><NavLink to="/about">About</NavLink></li>
@@ -20,6 +30,11 @@ export const Header = () => {
                             <li><NavLink to="/contact">Contact</NavLink></li>
                         </ul>
                     </nav>
+                    <div className="han-menu">
+<button onClick={handleButtonToggle}>
+    <GiHamburgerMenu />
+</button>
+                    </div>
                 </div>
             </div>
         </header>
