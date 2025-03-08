3517d4d532997b75eb854418d58654613077be32
AkashThakur14 <ak9988385532@gmail.com>
2025-03-09 00:30:31 +0530

    country page

--- src/components/UI/Footer.jsx
+++ src/components/UI/Footer.jsx
@@ -1,27 +1,29 @@
-import { IoCallSharp } from "react-icons/io5";
 import footerContact from "../../api/footerApi.json";
+import { IoCallSharp } from "react-icons/io5";
 import { TbMailPlus } from "react-icons/tb";
 import { MdPlace } from "react-icons/md";
 
 export const Footer = () => {
-
     const footerIcon = {
         MdPlace: <MdPlace />,
         IoCallSharp: <IoCallSharp />,
-        TbMailPlus:<TbMailPlus />
-    }
+        TbMailPlus: <TbMailPlus />
+    };
+
     return (
         <footer className="footer-section">
             <div className="footer-wrapper container">
                 {footerContact.map((curData, index) => {
-                    const { icon, title, details } = curData;
+                    const { icon, title, details, link } = curData;
                     return (
                         <div className="footer-contact" key={index}>
-                            <span className="icon">{footerIcon[icon]}</span>
-                            <div className="footer-text">
-                                <h4>{title}</h4>
-                                <p>{details}</p>
-                            </div>
+                            <a href={link} target="_blank" rel="noopener noreferrer" className="footer-link">
+                                <span className="icon">{footerIcon[icon]}</span>
+                                <div className="footer-text">
+                                    <h4>{title}</h4>
+                                    <p>{details}</p>
+                                </div>
+                            </a>
                         </div>
                     );
                 })}
--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,8 +1,46 @@
-export const Country = () =>{
+import { useEffect, useState, useTransition } from "react";
+import { getCountryData } from "../api/postApi";
+import {CountryCard} from "../components/layout/CountryCard"
+
+export const Country = () => {
+    const [isPending, startTransition] = useTransition();
+    const [countries, setCountries] = useState([]);
+
+
+    useEffect(() => {
+
+        startTransition(async () => {
+            const res = await getCountryData();
+            //  console.log(res);
+            setCountries(res.data);
+        })
+    }, []);
+
+
+
+    if (isPending) {
+        return (
+            <div className="loader"></div>
+        )
+    }
+
 
     return (
-        <>
-        <h1>Country Page</h1>
-        </>
+        <section className="country-section container">
+            
+
+                <ul className="countries-card four-col">
+                    {
+                        countries.map((currentCountry,index) => {
+                            return(
+                                <CountryCard country = {currentCountry} key={index} />
+                        )
+
+                        })
+                    }
+                </ul>
+
+        </section>
+
     )
 }
