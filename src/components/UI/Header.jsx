3914354e7e575f236a0587a93609b02e4ec7535b
AkashThakur14 <ak9988385532@gmail.com>
2025-03-09 02:26:11 +0530

    minor change

--- src/components/UI/Header.jsx
+++ src/components/UI/Header.jsx
@@ -3,37 +3,39 @@ import { NavLink } from "react-router-dom"
 import { useState } from "react";
 
 export const Header = () => {
-    const [show,setShow] = useState(false);
+    const [show, setShow] = useState(false);
 
-const handleButtonToggle = () =>{
-return(
-    setShow(!show)
-)
+    const handleButtonToggle = () => {
+        return (
+            setShow(!show)
+        )
+    }
+const handleCloseMenu = () =>{
+    setShow(false);
 }
-
     return (
 
         <header>
             <div className="container">
                 <div className="flex navbar-flex">
                     <div className="logo">
-                        <NavLink to="/">
+                        <NavLink to="/" onClick={handleCloseMenu}>
                             <h1>WorldAtlas</h1>
                         </NavLink>
                     </div>
 
                     <nav className={show ? "menu-mobile" : "menu-web"}>
                         <ul>
-                            <li><NavLink to="/">Home</NavLink></li>
-                            <li><NavLink to="/about">About</NavLink></li>
-                            <li><NavLink to="/country">Country</NavLink></li>
-                            <li><NavLink to="/contact">Contact</NavLink></li>
+                            <li><NavLink to="/" onClick={handleCloseMenu}>Home</NavLink></li>
+                            <li><NavLink to="/about" onClick={handleCloseMenu}>About</NavLink></li>
+                            <li><NavLink to="/country" onClick={handleCloseMenu}>Country</NavLink></li>
+                            <li><NavLink to="/contact" onClick={handleCloseMenu}>Contact</NavLink></li>
                         </ul>
                     </nav>
                     <div className="han-menu">
-<button onClick={handleButtonToggle}>
-    <GiHamburgerMenu />
-</button>
+                        <button onClick={handleButtonToggle}>
+                            <GiHamburgerMenu />
+                        </button>
                     </div>
                 </div>
             </div>
